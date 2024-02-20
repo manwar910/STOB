e0359bec4925e95e5a3447b32f8deab1e659bd2e
Malik Touqir anwar <69323552+manwar910@users.noreply.github.com>
2024-02-20 17:18:49 +0500

    Add files via upload

---
+++ STOBWeb/Scripts/Home/index.js
@@ -0,0 +1,165 @@
+﻿$().ready(function () {
+    $("#namecombobox").change(function () {
+        SessionTime();
+    });
+
+    SetupComboBoxes();
+
+    $("#combobox").combobox();
+    $("#combobox2").combobox();
+    $("#namecombobox").combobox();
+
+    $("#combobox").change(function (event) {
+        PopulateActivity2(event.target.value);
+    });
+});
+
+function PopulateActivity2(OptionSelected) {
+    // Polpulate combobox2 with all the available options except the one chosen in combobox by dropping the list of items in the combobox2, cloning combobox1, then removing
+    // the item that was passed as the item selected in combobox
+    //remove text of previously selected item from combobox2
+    $("select#combobox2").next("span.custom-combobox").children("input")[0].value = "";
+    // Drop the comboxbox2 options
+    document.getElementById('combobox2').innerHTML = "";
+    // Clone comboxbox into combobox2
+    $('#combobox option').clone().appendTo('#combobox2');
+    //remove the option that is passed
+    $("#combobox2 option[value='" + OptionSelected + "']").remove();
+}
+
+function SessionTime() {
+    $("#SignInDate").val(new Date().toLocaleString());
+}
+
+function SetupComboBoxes() {
+    $.widget("custom.combobox", {
+        _create: function () {
+            this.wrapper = $("<span>")
+                .addClass("custom-combobox")
+                .insertAfter(this.element);
+            
+            this.element.hide();
+            this._createAutocomplete();
+            this._createShowAllButton();
+        },
+
+        _createAutocomplete: function () {
+            var selected = this.element.children(":selected"),
+                value = selected.val() ? selected.text() : "";
+
+            this.input = $("<input>")
+                .appendTo(this.wrapper)
+                .val(value)
+                .attr("title", "")
+                .addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left")
+                .autocomplete({
+                    delay: 0,
+                    minLength: 0,
+                    source: $.proxy(this, "_source")
+                })
+                .tooltip({
+                    classes: {
+                        "ui-tooltip": "ui-state-highlight"
+                    }
+                });
+
+            this._on(this.input, {
+                autocompleteselect: function (event, ui) {
+                    ui.item.option.selected = true;
+                    $(ui.item.option.parentNode).trigger("change", event);
+                    this._trigger("select", event, {
+                        item: ui.item.option
+                    });
+                },
+
+                autocompletechange: "_removeIfInvalid"
+            });
+        },
+
+        _createShowAllButton: function () {
+            var input = this.input,
+                wasOpen = false;
+
+            $("<a>")
+                .attr("tabIndex", -1)
+                .tooltip()
+                .append("<span class='ui-button-icon ui-icon ui-icon-triangle-1-s'></span><span class='ui-button-icon-space'></span>")
+                .appendTo(this.wrapper)
+                .button({
+                    icons: {
+                        primary: "ui-icon-triangle-1-s"
+                    },
+                    text: false
+                })
+                .removeClass("ui-corner-all")
+                .addClass("ui-button ui-widget ui-button-icon-only custom-combobox-toggle ui-corner-right")
+                .on("mousedown", function () {
+                    wasOpen = input.autocomplete("widget").is(":visible");
+                })
+                .on("click", function () {
+                    input.trigger("focus");
+
+                    // Close if already visible
+                    if (wasOpen) {
+                        return;
+                    }
+
+                    // Pass empty string as value to search for, displaying all results
+                    input.autocomplete("search", "");
+                });
+        },
+
+        _source: function (request, response) {
+            var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+            response(this.element.children("option").map(function () {
+                var text = $(this).text();
+                if (this.value && (!request.term || matcher.test(text)))
+                    return {
+                        label: text,
+                        value: text,
+                        option: this
+                    };
+            }));
+        },
+
+        _removeIfInvalid: function (event, ui) {
+
+            // Selected an item, nothing to do
+            if (ui.item) {
+                return;
+            }
+
+            // Search for a match (case-insensitive)
+            var value = this.input.val(),
+                valueLowerCase = value.toLowerCase(),
+                valid = false;
+            this.element.children("option").each(function () {
+                if ($(this).text().toLowerCase() === valueLowerCase) {
+                    this.selected = valid = true;
+                    return false;
+                }
+            });
+
+            // Found a match, nothing to do
+            if (valid) {
+                return;
+            }
+
+            // Remove invalid value
+            this.input
+                .val("")
+                .attr("title", value + " didn't match any item")
+                .tooltip("open");
+            this.element.val("");
+            this._delay(function () {
+                this.input.tooltip("close").attr("title", "");
+            }, 2500);
+            this.input.autocomplete("instance").term = "";
+        },
+
+        _destroy: function () {
+            this.wrapper.remove();
+            this.element.show();
+        }
+    });
+}
