937b9e65db898c253939d24b06a8521a2ce85ed3
Malik Touqir anwar <69323552+manwar910@users.noreply.github.com>
2024-02-20 17:22:42 +0500

    Add files via upload

---
+++ STOBWeb/Scripts/Reference/RefAnncmnt/Index.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    $('#AnncmntTable').DataTable();
+});
---
+++ STOBWeb/Scripts/Reference/RefGymUsers/Create.js
@@ -0,0 +1,20 @@
+﻿function ADPLookup()
+{
+    var ADPEmployeeId = $("#ADPEmployeeId").val();
+    $.post(url, { emp: ADPEmployeeId }, function (data) {
+        if (data.ADPUser == null) {
+            alert("Invalid Employee ID entered. Please enter a valid Employee ID and try again.")
+        }
+        else {
+            $("#FirstName").val(data.ADPUser.FirstName[0].toUpperCase() + data.ADPUser.FirstName.substr(1).toLowerCase());
+         
+            $("#LastName").val(data.ADPUser.LastName[0].toUpperCase() + data.ADPUser.LastName.substr(1).toLowerCase());
+            $("#createbtn").removeAttr('disabled');
+            
+        }
+        //$(document).ready(function () {
+        //    $('#mytable').DataTable();
+        //});
+
+    });
+}
